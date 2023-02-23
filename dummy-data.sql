27b799c4fbdf3fb07640c3b2b046f920b624f13d
Oarabile Moima <oarabile@bbd.co.za>
2023-02-23 12:06:41 +0200

    User start data

---
+++ dummy-data.sql
@@ -0,0 +1,17 @@
+INSERT INTO [user](username, deleted)
+VALUES ('matt', 0),
+('jade', 0),
+('morgan', 0),
+('oara', 0);
+
+INSERT INTO [userEmail](email, userID)
+VALUES ('matt@gmail.com', 1),
+('jade@gmail.com', 2),
+('morgan@bbd.com', 3),
+('oiasuifoksdhfsadfsa@woohoo.com', 4);
+
+INSERT INTO [userRating](userID, rating)
+VALUES (1, 5),
+(2, 4),
+(3, 3),
+(4, 2);
